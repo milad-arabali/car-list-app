2b4003f17f973d6dd7c4185a2bdaf19d42229100
milad <miladarabali1@gmail.com>
2025-03-09 19:36:42 +0330

    #create-project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen bg-amber-300">ddd</div>
+        </>
+    )
+}
+
+export default App
