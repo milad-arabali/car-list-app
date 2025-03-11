ef28cdf0e84d090b63fec4fbb53b4f9ff418c12e
milad <miladarabali1@gmail.com>
2025-03-11 18:28:21 +0330

    #create-design-layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 function Footer(props) {
     return (
-        <div>footer</div>
+        <div
+            className="flex justify-center items-center w-[60%] h-[90px] mx-auto bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg mt-5 mb-5">
+            <p className="text-sm text-gray-700 text-white ">
+                <a
+                    href="https://farazcode.ir"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mr-5 text-white font-semibold hover:underline hover:text-blue-800 transition duration-300"
+                >
+                    farazcode.ir
+                </a>
+                ساخته شده توسط
+            </p>
+        </div>
     );
 }
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Main(props) {
     return (
-        <div>main</div>
+        <div className="flex flex-col min-h-[590px] ">
+            <div className="container mx-auto w-[60%] h-[90px] bg-gray-200 p-5 text-center mt-15 rounded-lg">
+                محتوای شروع‌شده از بالا
+            </div>
+        </div>
     );
 }
 
